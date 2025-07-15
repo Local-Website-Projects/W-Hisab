919e29ac779e3ce954c9a26ce4cbe7055be22ef4
Mugdho <saha2171515@stud.kuet.ac.bd>
2025-07-15 15:25:00 +0600

    15/07/2025

--- resources/views/debitCredit/index.blade.php
+++ resources/views/debitCredit/index.blade.php
@@ -170,7 +170,7 @@
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <table class="table table-responsive-md">
+                                    <table class="table table-bordered table-responsive-md">
                                         <thead>
                                         <tr>
                                             <th>Project Name</th>
--- resources/views/profileDeEx/index.blade.php
+++ resources/views/profileDeEx/index.blade.php
@@ -87,7 +87,7 @@
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <table class="table table-responsive-md table-bordered">
+                                    <table class="table table-bordered table-responsive-sm">
                                         <thead>
                                         <tr>
                                             <th>Date</th>
